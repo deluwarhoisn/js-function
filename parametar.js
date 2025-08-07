b40f09be87efeb671f43ca6e05e4568a6c1af8dd
deluwarhoisn <babuhossen301@gmail.com>
2025-08-07 21:59:20 +0600

    parametar

---
+++ parametar.js
@@ -0,0 +1,35 @@
+;
+function vaatKhao(){
+    console.log('hand wash kore aso');
+    console.log('plate e khabar dhabar nao');
+    console.log('gopat gopat kore khao');
+}
+// vaatKhao();
+
+//3 ---> 3*3 = 9
+//4 ---> 4*4 = 16
+// 5 --> 5*5 = 25
+
+function square (number){
+    console.log('value of the number parameter' ,number);
+    const borgo = number * number;
+    console.log('square of the given number is:', borgo);
+}
+square(512)
+console.log('----------')
+square(12);
+square(5);
+square(405);
+
+function add(num1,num2){
+    const sum =num1 + num2;
+    console.log(sum)
+}
+add(12,22);
+
+
+function addAll(a,b,c,d,e){
+    const total = a+b+c+d+e;
+    console.log(total)
+}
+addAll(2,5,9,4,1);
