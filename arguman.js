a4792fa7d42a1eb7fe78a779ce12987a9a24e7e6
deluwarhoisn <babuhossen301@gmail.com>
2025-08-07 22:23:54 +0600

    differnce

---
+++ arguman.js
@@ -0,0 +1,27 @@
+function dobleit (number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+
+console.log('i will call the function')
+dobleit(15);
+console.log('----------------')
+dobleit(88);
+console.log('----------------')
+dobleit(873);
+
+const number = 55;
+dobleit(number);
+const money =121;
+dobleit(money);
+
+
+//
+ function difference (num1, num2){
+    const diff = num1 - num2;
+    console.log(num1,num2, 'difference', diff)
+ }
+ const fatherAge = 40;
+ const myAge = 10;
+
+ difference(fatherAge,myAge);
