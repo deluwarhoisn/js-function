c391e100aa056492c7aab2ce362efd375c2d63d8
deluwarhoisn <babuhossen301@gmail.com>
2025-08-07 20:54:59 +0600

    function-javascript

---
+++ function.js
@@ -0,0 +1,32 @@
+const price = 45;
+
+function fanOffkor(){
+ console.log('bosa theke uthe dara')
+ console.log('walk towards  the switch');
+ console.log('click the switch to off of the Fan');
+}
+
+
+//call the function
+fanOffkor();
+
+
+// function name(){
+
+//}
+
+function brushTeeth(){
+ console.log('pivk up the brush')
+ console.log('add paste')
+ console.log('2 min goso and goso and fana felo')
+}
+
+// call the function
+
+brushTeeth();
+
+console.log('hjdsbdab jf bj')
+console.log('hjdsbdab jf bj')
+console.log('hjdsbdab jf bj')
+console.log('hjdsbdab jf bj')
+brushTeeth();
